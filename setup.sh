fd673f3502c5bea72691ff1f265b52396d5360b2
Moritz Roessler <morrossl@publicisgroupe.net>
2022-09-15 15:49:49 +0200

    fix: move to prod dependencies

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@ set -x
 
 yarn 
 mkdir -p public/repl
+mkdir -p public/monaco/min
+mkdir -p public/inline-module
+
 cp node_modules/inline-module/index.js public/repl/inline-module.js
 cp node_modules/@babel/standalone/babel.min.js public/repl/babel.min.js
 
