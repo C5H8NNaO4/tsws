87c102584aac8965f294ed149e87c46db17b2f2a
Moritz Roessler <morrossl@publicisgroupe.net>
2022-09-15 15:41:07 +0200

    feat:add build script

--- setup.sh
+++ setup.sh
@@ -7,4 +7,4 @@ mkdir -p public/repl
 cp node_modules/inline-module/index.js public/repl/inline-module.js
 cp node_modules/@babel/standalone/babel.min.js public/repl/babel.min.js
 
-cp -R node_modules/monaco-editor/min public/monaco/
+cp -R node_modules/monaco-editor/min public/monaco/min
